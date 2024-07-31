aef9d637738abe9dedf9a0ff18133cfda148a088
Ivan Bobynets <ivan@osyah.com>
2024-07-31 15:28:58 +0300

    container: add Closer, Base.Close, Base.RegisterCloser and rename Base.Register

--- container/README.md
+++ container/README.md
@@ -10,6 +10,8 @@ go get -u github.com/osyah/hryzun
 
 ## Example
 
+### Basic
+
 ```go
 type Service interface {
     Send(string)
@@ -22,10 +24,41 @@ func (UserService) Send(string) { /* ... */ }
 func main() {
     base := container.New()
 
-    base.Register("user_service", func(*container.Base) any {
+    base.RegisterHandler("user_service", func(*container.Base) any {
         return &UserService{}
     })
 
     client := container.Get[Service](base, "user_service")
+}
+```
+
+### With closer
+
+```go
+type Database interface {
+    Close() error
+}
+
+type Postgres struct{}
+
+func (Postgres) Close() error { /* ... */ }
+
+func main() {
+    base := container.New()
+
+    base.RegisterHandler("postgres", func(b *container.Base) any {
+        var postgres Postgres
+
+        b.RegisterCloser("postgres", postgres.Close)
+
+        return &postgres
+    })
+
+    postgres := container.Get[Database](base, "postgres")
+    // ...
+
+    if err := base.Close(); err != nil {
+        // ...
+    }
 }
 ```
--- container/base.go
+++ container/base.go
@@ -3,28 +3,53 @@
 
 package container
 
-type Handler func(*Base) any
+import (
+	"errors"
+	"fmt"
+)
 
-type Base struct{ m map[string]any }
+type (
+	Handler func(*Base) any
+	Closer  func() error
+)
 
-func New() *Base {
-	return &Base{m: make(map[string]any)}
+type Base struct {
+	h map[string]any
+	c map[string]Closer
 }
 
-func (b Base) Register(name string, handler Handler) { b.m[name] = handler }
+func New() *Base {
+	return &Base{h: make(map[string]any), c: make(map[string]Closer)}
+}
+
+func (b Base) RegisterHandler(name string, handler Handler) { b.h[name] = handler }
+
+func (b Base) RegisterCloser(name string, closer Closer) { b.c[name] = closer }
 
 func (b *Base) Get(name string) any {
-	value, ok := b.m[name]
+	value, ok := b.h[name]
 	if !ok {
 		panic("hryzun/container: invalid name")
 	}
 
 	if handler, ok := value.(Handler); ok {
 		value = handler(b)
-		b.m[name] = value
+		b.h[name] = value
 	}
 
 	return value
+}
+
+func (b Base) Close() error {
+	var group error
+
+	for name, closer := range b.c {
+		if err := closer(); err != nil {
+			group = errors.Join(group, fmt.Errorf("%s: %s", name, err))
+		}
+	}
+
+	return group
 }
 
 func Get[T any](base *Base, name string) T { return base.Get(name).(T) }
